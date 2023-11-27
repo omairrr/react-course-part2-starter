a67523b81946a94ae0d72b96ea6ce970f1d80109
umair <87530550+omairrr@users.noreply.github.com>
2023-11-27 13:55:20 +0500

    dsfsd

---
+++ src/react-query/Hooks/UsePosts.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+interface PostsQuery {
+  page: number;
+  pageSize: number;
+}
+const UsePosts = (query: PostsQuery) => {
+  const fetchPosts = () =>
+    axios
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
+        },
+      })
+      .then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    queryKey: ["posts", query],
+    queryFn: fetchPosts,
+    staleTime: 1 * 60 * 1000,
+    keepPreviousData: true,
+  });
+};
+
+export default UsePosts;
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,35 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import UsePosts from "./Hooks/UsePosts";
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  const { data: posts, error, isLoading } = UsePosts({ page, pageSize });
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
+  if (isLoading) return <p>wait please</p>;
 
   return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <button
+        disabled={page === 1}
+        className="btn btn-primary"
+        onClick={() => setPage(page - 1)}
+      >
+        Previous
+      </button>
+      <button className="btn btn-primary" onClick={() => setPage(page + 1)}>
+        Next
+      </button>
+    </>
   );
 };
 
